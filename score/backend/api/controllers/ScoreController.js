29674075a1dbb0b891e31cacad23778edea422d5
maro <martin.ronckevic@mimacom.com>
2014-07-12 20:57:21 +0200

    added checked post. closes #1

--- score/backend/api/controllers/ScoreController.js
+++ score/backend/api/controllers/ScoreController.js
@@ -16,8 +16,56 @@
  */
 
 module.exports = {
-    
-  
+
+    checkedCreate: function(req, res) {
+        console.log('checkedCreate');
+        Score.findOne({
+                where: {
+                    playerId: req.param('playerId'),
+                    level: req.param('level')
+                }
+            }
+        ).exec(function (err, score) {
+            console.log('checkedCreate - find finished');
+            if (err) return res.send(err,500);
+            if (!score) {
+                console.log('no score with playerId: ' + req.param('playerId') + ' level: ' + req.param('level'));
+                Score.create({
+                    "playerId": req.param('playerId'),
+                    "playerName": req.param('playerName'),
+                    "scoreType": req.param('scoreType'),
+                    "score": req.param('score'),
+                    "time": req.param('time'),
+                    "level": req.param('level')
+                }).done(function(err, user) {
+                    console.log('checkedCreate - score creation done');
+                    if (err) return res.send(err,500);
+                    res.json(score);
+                });
+            }
+            console.log('checkedCreate - there is score: ' + JSON.stringify(score));
+            console.log(score.score + ' ' + req.param('score'));
+            if (score.score < req.param('score')) {
+                console.log('checkedCreate - updating score value');
+                score.score = req.param('score');
+                score.time = req.param('time');
+                // Persist the change
+                score.save(function (err) {
+                    if (err) return res.send(err,500);
+
+                    // Report back with the new state of the score
+                    res.json(score);
+                });
+            } else {
+                console.log('checkedCreate - not updating score');
+                res.json(score);
+            }
+
+
+
+        });
+
+    },
 
 
   /**
