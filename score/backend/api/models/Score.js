15d3bd4e6885b4644983bbb82df420bd8b9f8ccc
maro <martin.ronckevic@mimacom.com>
2014-07-12 20:48:03 +0200

    added time to score attributes

--- score/backend/api/models/Score.js
+++ score/backend/api/models/Score.js
@@ -3,17 +3,17 @@
  *
  * @module      :: Model
  * @description :: A short summary of how this model works and what it represents.
- * @docs		:: http://sailsjs.org/#!documentation/models
+ * @docs        :: http://sailsjs.org/#!documentation/models
  */
 
 module.exports = {
 
-  attributes: {
-    "playerId": 'STRING',
-    "playerName": 'STRING',
-    "scoreType": 'STRING',
-    "score": 'INT',
-    "level": 'STRING'
-  }
-
+    attributes: {
+        "playerId": 'STRING',
+        "playerName": 'STRING',
+        "scoreType": 'STRING',
+        "score": 'INT',
+        "time": 'INT',
+        "level": 'STRING'
+    }
 };
